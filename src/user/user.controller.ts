de4df84bae1acd0b2347c2b50674487276d5d814
Marcos Vinicyus <marcos.vinicyus.c.s@gmail.com>
2024-01-12 16:31:31 -0300

    uso dto e configurações

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -1,15 +1,18 @@
-import { Controller, Get, Post } from '@nestjs/common';
+import { Body, Controller, Get, Post } from '@nestjs/common';
+import { CreateUserDto } from 'src/dtos/createUser.dto';
 
 @Controller('user')
 export class UserController {
-    
-    @Get()
-    async getAllUsers() {
-        return JSON.stringify({test: 'test'});
-    }
+  @Get()
+  async getAllUsers() {
+    return JSON.stringify({ test: 'test' });
+  }
 
-    @Post()
-    async createUser() {
-        return JSON.stringify({test: 'test'});
-    }
+  @Post()
+  async createUser(@Body() createUser: CreateUserDto) {
+    return {
+      ...createUser,
+      password: '*********',
+    };
+  }
 }
