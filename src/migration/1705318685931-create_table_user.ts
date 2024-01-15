d70d33e7780d1e7c7b7aead404b90345cb1dfbe5
Marcos Vinícyus <marcos.vinicyus.c.s@gmail.com>
2024-01-15 10:56:23 -0300

    Correções, Cadastrando User

--- src/migration/1705318685931-create_table_user.ts
+++ src/migration/1705318685931-create_table_user.ts
@@ -6,18 +6,18 @@ export class CreateTableUser1705318685931 implements MigrationInterface {
         queryRunner.query(`
             CREATE TABLE public.user (
                 id integer NOT NULL,
-                name character varying(255) NOT NULL,
-                email character varying(255) NOT NULL,
-                password character varying(255) NOT NULL,
-                cpf character varying(255) NOT NULL,
-                phone character varying(255) NOT NULL,
+                name character varying NOT NULL,
+                email character varying NOT NULL,
+                phone character varying NOT NULL,
+                cpf character varying NOT NULL,
                 type_user int NOT NULL,
+                password character varying NOT NULL,
                 created_at TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
                 updated_at TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
                 primary key (id)
             );
 
-            Create SEQUENCE IF NOT EXISTS public.user_id_seq
+            CREATE SEQUENCE public.user_id_seq
                 AS integer
                 START WITH 1
                 INCREMENT BY 1
