32718a6d67ced38fce7c0103a5639f3740a86eea
Marcos Vinícyus <marcos.vinicyus.c.s@gmail.com>
2024-01-15 11:37:03 -0300

    Adicionando novas migrations, correções feitas

--- src/migration/1705327486166-create_table_state.ts
+++ src/migration/1705327486166-create_table_state.ts
@@ -1,23 +1,18 @@
 import { MigrationInterface, QueryRunner } from "typeorm";
 
-export class CreateTableUser1705318685931 implements MigrationInterface {
+export class CreateTableState1705327486166 implements MigrationInterface {
 
     public async up(queryRunner: QueryRunner): Promise<void> {
         queryRunner.query(`
-            CREATE TABLE IF NOT EXISTS public.user (
+            CREATE TABLE IF NOT EXISTS public.state (
                 id integer NOT NULL,
-                name varchar NOT NULL,
-                email varchar NOT NULL,
-                phone varchar NOT NULL,
-                cpf varchar NOT NULL,
-                type_user int NOT NULL,
-                password varchar NOT NULL,
+                name_character varchar NOT NULL,
                 created_at TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
                 updated_at TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
                 primary key (id)
             );
 
-            CREATE SEQUENCE public.user_id_seq
+            CREATE SEQUENCE public.state_id_seq
                 AS integer
                 START WITH 1
                 INCREMENT BY 1
@@ -25,16 +20,16 @@ export class CreateTableUser1705318685931 implements MigrationInterface {
                 NO MAXVALUE
                 CACHE 1;
 
-            ALTER SEQUENCE public.user_id_seq OWNED BY public.user.id;
+            ALTER SEQUENCE public.state_id_seq OWNED BY public.state.id;
 
-            ALTER TABLE ONLY public.user ALTER COLUMN id SET DEFAULT nextval('public.user_id_seq'::regclass);
-        `)
+            ALTER TABLE ONLY public.state ALTER COLUMN id SET DEFAULT nextval('public.state_id_seq'::regclass);
+        `);
     }
 
     public async down(queryRunner: QueryRunner): Promise<void> {
         queryRunner.query(`
-            DROP TABLE IF EXISTS public.user;
-        `)
+            DROP TABLE public.state;
+        `);
     }
 
 }
