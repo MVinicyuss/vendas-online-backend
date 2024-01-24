63bdcbd3441025310d7a3982f7e6a7b4158bad58
Marcos Vinicyus <marcos.vinicyus.c.s@gmail.com>
2024-01-23 21:23:03 -0300

    Criando modulo de cache, possibilitando o uso globalmente

---
+++ src/cache/cache.service.ts
@@ -0,0 +1,27 @@
+import { CACHE_MANAGER } from '@nestjs/cache-manager';
+import { Cache } from 'cache-manager';
+import { Inject, Injectable } from '@nestjs/common';
+
+@Injectable()
+export class CacheService {
+  constructor(
+    @Inject(CACHE_MANAGER)
+    private cacheManager: Cache,
+  ) {}
+
+  async getCache<T>(
+    key: string,
+    functionRequest: () => Promise<T>,
+  ): Promise<T> {
+    let allData: T = await this.cacheManager.get(key);
+
+    if (allData) {
+      return allData;
+    }
+
+    allData = await functionRequest();
+    await this.cacheManager.set(key, allData);
+
+    return allData;
+  }
+}
--- src/city/city.module.ts
+++ src/city/city.module.ts
@@ -1,17 +1,12 @@
 import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
-import { CacheModule } from '@nestjs/cache-manager';
 import { CityEntity } from './entities/city.entity';
 import { CityController } from './city.controller';
 import { CityService } from './city.service';
+import { CacheModule as CacheModuleNest } from 'src/cache/cache.module';
 
 @Module({
-  imports: [
-    CacheModule.register({
-      ttl: 90000
-    }),
-    TypeOrmModule.forFeature([CityEntity]),
-  ],
+  imports: [TypeOrmModule.forFeature([CityEntity]), CacheModuleNest],
   controllers: [CityController],
   providers: [CityService],
 })
