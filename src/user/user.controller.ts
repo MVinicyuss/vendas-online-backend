1e388195034ee8b89c89ad32301b139019f03d9f
Marcos Vinícyus <marcos.vinicyus.c.s@gmail.com>
2024-01-24 11:28:03 -0300

    Adicionando dto no POST e na resposta

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -1,15 +1,18 @@
-import { Body, Controller, Get, Post } from '@nestjs/common';
+import { Body, Controller, Get, Post, UsePipes, ValidationPipe } from '@nestjs/common';
 import { CreateUserDto } from 'src/user/dtos/createUser.dto';
 import { UserService } from './user.service';
+import { ReturnUserDto } from './dtos/returnUser.dto';
+import { UserEntity } from './entities/user.entity';
 
 @Controller('user')
 export class UserController {
   constructor(private readonly userService: UserService) {}
   @Get()
-  async getAllUser() {
-    return this.userService.getAllUser();
+  async getAllUser(): Promise<ReturnUserDto[]>{
+    return (await this.userService.getAllUser()).map((UserEntity) => new ReturnUserDto(UserEntity));
   }
 
+  @UsePipes(ValidationPipe)
   @Post()
   async createUser(@Body() createUser: CreateUserDto) {
     return this.userService.createUser(createUser);
