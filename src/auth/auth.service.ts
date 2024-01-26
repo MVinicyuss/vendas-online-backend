36943b40f3e63ffa3520bd1fd8c144dbdcbab33c
Marcos Vinícyus <marcos.vinicyus.c.s@gmail.com>
2024-01-26 10:26:25 -0300

    Adicionando modulo de autenticação de user

---
+++ src/auth/auth.service.ts
@@ -0,0 +1,28 @@
+import { Injectable, NotFoundException } from '@nestjs/common';
+import { LoginUserDto } from './dtos/login.dto';
+import { UserService } from 'src/user/user.service';
+import { UserEntity } from 'src/user/entities/user.entity';
+import { hash } from 'bcrypt';
+
+@Injectable()
+export class AuthService {
+    constructor(
+        private readonly userService: UserService,
+    ){}
+
+    async login(loginUserDto: LoginUserDto): Promise<UserEntity> {
+        const user: UserEntity = await this.userService.findUserByEmail(loginUserDto.email).catch(() => undefined);
+
+        if(!user){
+            throw new NotFoundException('Email not found');
+        }
+        
+        const isPasswordValid = await hash(loginUserDto.password, user?.password);
+         
+        if(!isPasswordValid){
+            throw new Error('Email and Password not match');
+        }
+
+        return user;
+    }
+}
