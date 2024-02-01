c6e71ce69cd13080338a598302a60c6f32b2e790
Marcos Vinícyus <marcos.vinicyus.c.s@gmail.com>
2024-02-01 12:05:47 -0300

    Adicionando mais testes

---
+++ src/user/__mocks__/createUser.mock.ts
@@ -0,0 +1,9 @@
+import { CreateUserDto } from '../dtos/createUser.dto';
+
+export const createUserDtoMock: CreateUserDto = {
+  cpf: '12345678901',
+  email: 'emailmock@gmail.com',
+  name: 'name mock',
+  password: 'passwordMock',
+  phone: '12345678901',
+};
--- src/user/__tests__/user.service.spec.ts
+++ src/user/__tests__/user.service.spec.ts
@@ -4,6 +4,7 @@ import { Repository } from 'typeorm';
 import { UserEntity } from '../entities/user.entity';
 import { getRepositoryToken } from '@nestjs/typeorm';
 import { userEntityMock } from '../__mocks__/user.mock';
+import { createUserDtoMock} from '../__mocks__/createUser.mock';
 
 describe('UserService', () => {
   let service: UserService;
@@ -16,8 +17,8 @@ describe('UserService', () => {
         {
           provide: getRepositoryToken(UserEntity),
           useValue: {
-            findOne: jest.fn().mockResolvedValue({ userEntityMock }),
-            save: jest.fn().mockResolvedValue({ userEntityMock }),
+            findOne: jest.fn().mockResolvedValue( userEntityMock ),
+            save: jest.fn().mockResolvedValue( userEntityMock ),
           },
         },
       ],
@@ -33,4 +34,51 @@ describe('UserService', () => {
     expect(service).toBeDefined();
     expect(userRepository).toBeDefined();
   });
+
+  it('should return user in findUserByEmail', async () => {
+    const user = await service.findUserByEmail(userEntityMock.email);
+    expect(user).toEqual(userEntityMock);
+  });
+
+  it('should return error in findUserByEmail', async () => {
+    jest.spyOn(userRepository, 'findOne').mockResolvedValue(undefined);
+    expect(service.findUserByEmail(userEntityMock.email)).rejects.toThrow();
+  });
+
+  it('should return error in findUserByEmail (error DB)', async () => {
+    jest.spyOn(userRepository, 'findOne').mockRejectedValue(new Error());
+    expect(service.findUserByEmail(userEntityMock.email)).rejects.toThrow();
+  });
+
+  it('should return user in getUserById', async () => {
+    const user = await service.getUserById(userEntityMock.id);
+    expect(user).toEqual(userEntityMock);
+  });
+
+  it('should return error in getUserById', async () => {
+    jest.spyOn(userRepository, 'findOne').mockResolvedValue(undefined);
+    expect(service.getUserById(userEntityMock.id)).rejects.toThrow();
+  });
+
+  it('should return error in getUserById (error DB)', async () => {
+    jest.spyOn(userRepository, 'findOne').mockRejectedValue(new Error());
+    expect(service.getUserById(userEntityMock.id)).rejects.toThrow();
+  });
+
+  it('should return user in getUserByIdWithRelations', async () => {
+    jest.spyOn(userRepository, 'findOne').mockResolvedValue(undefined);
+    expect(service.getUserByIdWithRelations(userEntityMock.id)).rejects.toThrow();
+  });
+
+  it('should return error if user exists', async () => {
+    expect(service.createUser(createUserDtoMock)).rejects.toThrow(new Error());
+  });
+
+  it('should return user if user not exists', async () => {
+    jest.spyOn(userRepository, 'findOne').mockResolvedValue(undefined);
+
+    const user = await service.createUser(createUserDtoMock);
+    expect(user).toEqual(userEntityMock);
+  });
+
 });
